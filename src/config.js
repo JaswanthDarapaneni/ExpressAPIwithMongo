3cad17982aac75b00f384a79e5740d8e1ac7d123
jaswanthkrish1 <jaswanth.d304@gmail.com>
2024-04-08 21:04:44 +0530

    dbInfoAdded

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   secretKey: 'SuperManisMyhero',
-  databaseURL: process.env.MONGODB_URL || 'mongodb://localhost:27017/mydb'
+  databaseURL: process.env.MONGODB_URL || 'mongodb+srv://jaswanthd304:y6AKC24hCGj8nsE5@cluster.pubsvhl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster'
 };
 
 module.exports = config;
