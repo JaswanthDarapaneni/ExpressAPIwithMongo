8f00b6233b2a0f59a657138618f0c2214d0db143
jaswanthkrish1 <jaswanth.d304@gmail.com>
2024-04-08 21:11:04 +0530

    test

--- src/config.js
+++ src/config.js
@@ -2,5 +2,4 @@ const config = {
   secretKey: 'SuperManisMyhero',
   databaseURL: process.env.MONGODB_URL || 'mongodb+srv://jaswanthd304:y6AKC24hCGj8nsE5@cluster.pubsvhl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster'
 };
-
 module.exports = config;
